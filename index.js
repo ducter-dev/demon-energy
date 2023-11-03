87a45e78336cfa5ffd083dc1e00df0bcf53abc3b
ducterdev <michel.morales@ducter.com.mx>
2023-11-03 12:03:53 -0600

    :sparkles: Agregada función para obtener subgrupos

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 require('dotenv').config()
 const mysql = require('mysql')
 const axios = require('axios')
+var colors = require('colors')
+
+
+colors.enable()
 
 const port = process.env.PORT_DB_TPA
 const dbHostTPA = process.env.HOST_DB_TPA
@@ -17,8 +21,8 @@ const dbDatabaseIRGE = process.env.DB_IRGE
 const api_url = process.env.API_URL
 
 async function monitorearEnergy() {
-  
-  await getNominaciones()
+  console.log('Iniciando monitoreo de energy24-7'.bgBlue)
+  await conectarTPA()
   setTimeout(monitorearEnergy, 10000)
 }
 
@@ -74,10 +78,65 @@ async function conectarIRGE() {
   })
 }
 
+async function getCustomersNews() {
+
+  // Obtener la información del api
+
+  await axios.get(`${api_url}/subgrupos`)
+    .then(response => {
+      const { data } = response
+      console.log(`${data}`.bgGreen)
+      
+      if (data.lenght > 0) {
+        const conexion = mysql.createConnection({
+          host: data.terminal === 'TPA' ? dbHostTPA : dbHostIRGE,
+          user: data.terminal === 'TPA' ? dbUserTPA : dbUserIRGE,
+          password: data.terminal === 'TPA' ? dbPasswordTPA : dbPasswordIRGE,
+          database: data.terminal === 'TPA' ? dbDatabaseTPA : dbDatabaseIRGE
+        })
+      
+        conexion.connect((error) => {
+          if (error) {
+            console.error(`Error al conectar a la base de datos:  ${error.stack}`.bgRed)
+            return null
+          }
+          const idCompania = data.compania == 1 ? data.compania : 3
+          const sql = `INSERT INTO subgrupos(id, orden, nombre, activo, compania, grupo)
+                      VALUES('${data.clave}', 0, '${data.nombre}', 1, ${idCompania}, 1)`
+          conexion.query(sql, (error, result) => {
+            if (error) {
+              console.error(`Error al realizar la inserción del subgrupo: ${error.stack}`.bgRed)
+              return null
+            }
+            console.log(`Creado en subgrupos: ${result}`.bgGreen)
+            const sql2 = `INSERT INTO nominaciones_orden(subgrupo, orden, color, textColor, label, button)
+                          VALUES('${data.clave}', 0, '#D1D1D1', 'black','black','#B3AEAE')`
+            conexion.query(sql2, (error, result) => {
+              if (error) {
+                console.error(`Error al realizar la inserción del subgrupo: ${error.stack}`.bgRed)
+                return null
+              }
+              console.log(`Agregeado al grupo de nominaciones: ${result}`.bgGreen)
+            })
+            // Enviara actualización del id de subgrupo
+            conexion.end()
+          })
+        })
+      } else {
+        console.log('No existen subgrupos nuevos.'.bgYellow)
+      }
+    })
+    .catch(error => {
+      console.log(`Error: ${error}`.bgRed)
+    })
+
+    //
+
+}
+
 
 async function getNominaciones ()
 {
-  console.log(`api_url: ${api_url}`)
   await axios.get(api_url)
     .then(response => {
       console.log(response.data)
